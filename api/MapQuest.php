382b3b347ed5e4be8bb375789e8b29cd2f6d0343
Aymeric ASSIER <a.assier@newquest.fr>
2013-05-07 15:41:00 +0200

    Unmute cURL errors

--- api/MapQuest.php
+++ api/MapQuest.php
@@ -153,18 +153,18 @@ class MapQuest implements API
     {
         $ch = curl_init();
 
-        @curl_setopt($ch, CURLOPT_URL, $url);
-        @curl_setopt($ch, CURLOPT_POST, false);
-        @curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        @curl_setopt($ch, CURLOPT_HEADER, false);
-        @curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        @curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        @curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        @curl_setopt($ch, CURLOPT_VERBOSE, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
 
-        $result = @curl_exec($ch);
+        $result = curl_exec($ch);
 
-        @curl_close($ch);
+        curl_close($ch);
 
         return $result ?: false;
     }
